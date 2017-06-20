67422867c3a48989e71642e044074a159e8cf13d
mdnyhus <mdnyhus@gmail.com>
2017-06-21 01:53:29 +0200

    added red circle, improved city list, and added score and popover

--- mapGame.js
+++ mapGame.js
@@ -3,10 +3,12 @@ var activeQuestion = true;
 var hints = 0;
 var maxHints = 2;
 
+var defaultZoom = 2;
+
 var map;
 var clickMarker;
 var cityMarker;
-var line;
+var circle;
 
 var curQuestion = {};
 
@@ -48,7 +50,7 @@ window.onload = function() {
   
   let mapProp = {
     center: new google.maps.LatLng(0, 0),
-    zoom: 2,
+    zoom: defaultZoom,
     
     streetViewControl: false,
     mapTypeId: google.maps.MapTypeId.TERRAIN,
@@ -75,6 +77,8 @@ window.onload = function() {
 }
 
 function loadQuestion() {
+  closePopover();
+  
   // setup map
   activeQuestion = true;
   map.setOptions({styles: blankMapStyles});
@@ -84,12 +88,12 @@ function loadQuestion() {
   if (cityMarker) {
     cityMarker.setMap(null);
   }
-  if (line) {
-    line.setMap(null);
+  if (circle) {
+    circle.setMap(null);
   }
   map.setOptions({
     center: new google.maps.LatLng(0, 0),
-    zoom: 2
+    zoom: defaultZoom
   });
   
   
@@ -102,6 +106,8 @@ function loadQuestion() {
   document.getElementById("bold-label-1").innerHTML = curQuestion.city;
   document.getElementById("normal-label-2").innerHTML = ""
   document.getElementById("bold-label-2").innerHTML = "";
+  document.getElementById("normal-label-3").innerHTML = ""
+  document.getElementById("bold-label-3").innerHTML = "";
 }
 
 function hintClick() {
@@ -121,33 +127,36 @@ function hintClick() {
   }
 }
 
+function closePopover() {
+  $("#popover").css("display", "");
+}
+
+function popoverClick(event) {
+  closePopover();
+}
+
+function popoverBodyClick(event) {
+  event.stopPropagation();
+}
+
 function guess() {
   activeQuestion = false;
   
   cityMarker = placeMarker({lat: curQuestion.lat, lng: curQuestion.lng});
   
-  // draw a dashed line between the two markers
-  var lineSymbol = {
-    path: 'M 0,-1 0,1',
-    strokeOpacity: 1,
-    scale: 4
-  };
+  var distanceM = Math.round(google.maps.geometry.spherical.computeDistanceBetween(
+    clickMarker.getPosition(), cityMarker.getPosition()));
   
-  console.log({lat: curQuestion.lat, lng: curQuestion.lng});
-  line = new google.maps.Polyline({
-    path: [
-      clickMarker.getPosition(),
-      cityMarker.getPosition(),
-    ],
+  circle = new google.maps.Circle({
+    map: map,
+    radius: distanceM,
+    strokeColor: '#000000',
     strokeOpacity: 0,
-    icons: [{
-      icon: lineSymbol,
-      offset: '0',
-      repeat: '20px'
-    }],
-    map: map
+    // strokeWeight: 2,
+    fillColor: '#FF0000',
+    fillOpacity: 0.25,
   });
-  console.log("b");
+  circle.bindTo('center', cityMarker, 'position');
   
   map.setOptions({styles: []});
   $("#primary-button").text("New Question");
@@ -156,15 +165,31 @@ function guess() {
   
   $("#secondary-button").css("display", "");
   
-  var distanceKm = Math.round(google.maps.geometry.spherical.computeDistanceBetween(
-    clickMarker.getPosition(), cityMarker.getPosition()) / 1000);
+  var distanceKm = Math.round(distanceM / 1000)
   
   document.getElementById("normal-label-1").innerHTML = "You were&nbsp;";
   document.getElementById("bold-label-1").innerHTML = distanceKm + "km";
   document.getElementById("normal-label-2").innerHTML = "&nbsp;away from&nbsp;"
   document.getElementById("bold-label-2").innerHTML = curQuestion.city + ", " + curQuestion.province + ", " + curQuestion.country;
+  document.getElementById("normal-label-3").innerHTML = "&nbsp;-&nbsp;Score:&nbsp;"
+  document.getElementById("bold-label-3").innerHTML = score(distanceKm, curQuestion.pop, hints);
   
+  document.getElementById("popover-score-bold").innerHTML = score(distanceKm, curQuestion.pop, hints);
+  document.getElementById("popover-distance-bold").innerHTML = distanceKm + "km";
+  document.getElementById("popover-pop-bold").innerHTML = curQuestion.pop;
+  document.getElementById("popover-hints-bold").innerHTML = hints
   
+  $("#popover").css("display", "flex");
+  console.log(cityMarker.position.lat())
+  console.log(clickMarker.position.lat())
+  if (cityMarker.position.lat() > clickMarker.position.lat()) {
+    // cityMarker is farther north
+    $("#popover-body").css("margin-top", "50px");
+    $("#popover-body").css("margin-bottom", "");
+  } else {
+    $("#popover-body").css("margin-top", "");
+    $("#popover-body").css("margin-bottom", "50px");
+  }
 }
 
 function placeMarker(location, color) {
@@ -189,9 +214,87 @@ function placeMarker(location, color) {
 
 // center map between click and city markers
 function centerMap() {
-  var bounds = new google.maps.LatLngBounds();
-  bounds.extend(clickMarker.position);
-  bounds.extend(cityMarker.position);
-  map.fitBounds(bounds);
+  // var bounds = new google.maps.LatLngBounds();
+  // bounds.extend(clickMarker.position);
+  // bounds.extend(cityMarker.position);
+  // map.fitBounds(bounds);
+  map.panTo(cityMarker.position);
+  map.fitBounds(circle.getBounds()); 
+  // smoothZoom(map.getZoom(), getZoomByBounds(circle.getBounds()), map.getZoom());
+}
+
+// the smooth zoom function
+// based on https://stackoverflow.com/questions/4752340/how-to-zoom-in-smoothly-on-a-marker-in-google-maps
+function smoothZoom (start, final, cur) {
+  console.log("smoothZoom");
+  console.log(start)
+  console.log(final);
+  console.log(cur);
+  if (cur == final) {
+      return;
+  }
+  else {
+    var change = 1;
+    if (start > final) {
+      change = -1;
+    }
+    z = google.maps.event.addListener(map, 'zoom_changed', function(event) { // or idle
+      google.maps.event.removeListener(z);
+      smoothZoom(start, final, cur + change);
+    });
+    setTimeout(function(){map.setZoom(cur)}); // 80ms is what I found to work well on my system -- it might not work well on all systems
+  }
+} 
+
+/**
+* Returns the zoom level at which the given rectangular region fits in the map view. 
+* The zoom level is computed for the currently selected map type. 
+* @param {google.maps.LatLngBounds} bounds 
+* @return {Number} zoom level
+*
+* based off https://stackoverflow.com/questions/9837017/equivalent-of-getboundszoomlevel-in-gmaps-api-3
+**/
+function getZoomByBounds(bounds){
+  console.log(bounds);
+  var MAX_ZOOM = map.mapTypes.get( map.getMapTypeId() ).maxZoom || 21 ;
+  var MIN_ZOOM = map.mapTypes.get( map.getMapTypeId() ).minZoom || 0 ;
+
+  var ne= map.getProjection().fromLatLngToPoint( bounds.getNorthEast() );
+  var sw= map.getProjection().fromLatLngToPoint( bounds.getSouthWest() ); 
+
+  var worldCoordWidth = Math.abs(ne.x-sw.x);
+  var worldCoordHeight = Math.abs(ne.y-sw.y);
+
+  //Fit padding in pixels 
+  var FIT_PAD = 40;
+
+  for( var zoom = MAX_ZOOM; zoom >= MIN_ZOOM; --zoom ){ 
+      if( worldCoordWidth*(1<<zoom)+2*FIT_PAD < $(map.getDiv()).width() && 
+          worldCoordHeight*(1<<zoom)+2*FIT_PAD < $(map.getDiv()).height() )
+          return zoom;
+  }
+  return 0;
+} 
+
+// score should be inverse to distance, inverse to population, and inverse to hints
+function score(distanceKm, population, hints) {
+  var distanceFactor = 500; // would be a mediocre/bad distance
+  var distancePow = 1.5; // should be most significant
+  var populationFactor = 683111 // median population
+  var populationPow = 0.5; // should be less significant
+  
+  var hintLoss = 0.2;
+  
+  var overallMultiplier = 100;
+  
+  var decimals = 2;
+  var decimalsFactor = Math.pow(10, decimals);
+  
+  // general idea: weight the distance/population score, then multiply some powered function that affects their signficance
+  // distance should be more significatn that population
+  return decimalsFactor * Math.round(overallMultiplier * 
+    Math.pow((distanceFactor / distanceKm), distancePow) * 
+    Math.pow((populationFactor / population), populationPow) * 
+    Math.pow(1 - hintLoss, hints) / decimalsFactor)
 }
 
